bf97cab0a56dc716868ff0f818ea75b5358d7342
b-long <b-long@users.noreply.github.com>
2025-05-31 17:20:20 -0400

    Small bit of cleanup

--- {{ cookiecutter.project_slug }}/setup.py
+++ {{ cookiecutter.project_slug }}/setup.py
@@ -14,15 +14,17 @@ Based on:
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
+PACKAGE_PATH = "g{{ cookiecutter.project_slug }}"
+PACKAGE_NAME = "{{ cookiecutter.project_slug }}"
 
 setuptools.setup(
-    name="{{ cookiecutter.project_slug }}",
-    packages=setuptools.find_packages(include=["{{ cookiecutter.project_slug }}"]),
-    py_modules=["{{ cookiecutter.project_slug }}.g{{ cookiecutter.project_slug }}"],
+    name=PACKAGE_NAME,
+    packages=setuptools.find_packages(include=[PACKAGE_NAME]),
+    py_modules=[f"{PACKAGE_NAME}.{PACKAGE_PATH}"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
-    package_data={"{{ cookiecutter.project_slug }}": ["*.so"]},
+    package_data={PACKAGE_NAME: ["*.so"]},
     # Should match 'pyproject.toml' version number
     version="{{ cookiecutter.version }}",
     author_email="{{ cookiecutter.author_email }}",
